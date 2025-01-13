292adbe6fe2be7ad2879d620b322e5a21251edff
Emilio <emiliojosebarco@gmail.com>
2025-01-13 18:11:02 +0100

    ejercicios a 13 de enero de 2025

---
+++ ejercicio4.js
@@ -0,0 +1,10 @@
+let edad;
+console.log("Voy a comprobar si eres mayor de edad");
+edad = prompt("Escriba una edad: ");
+if (edad > 17) {
+    alert("MAYOR DE EDAD \u0D6C");
+    console.log("Mayor de edad  \u0D6C");
+} else {
+    alert("MENOR DE EDAD \u0D6C");
+    console.log("Menor de edad  \u0D6C");
+}
